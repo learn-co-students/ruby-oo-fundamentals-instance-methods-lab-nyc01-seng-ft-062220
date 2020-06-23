f8e9f4c91713ac3d1899d8deeb7a37756b658081
kylejb <47502769+kylejb@users.noreply.github.com>
2020-06-23 11:50:30 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+# In this lab, we will practice defining classes and building instance methods inside them. 
+# Specifically, you are going to be adding 2 instance methods to a Dog class.
+
+class Dog 
+    def bark
+        puts "Woof!"
+    end
+    
+    def sit
+        puts "The Dog is sitting"
+    end
+end
